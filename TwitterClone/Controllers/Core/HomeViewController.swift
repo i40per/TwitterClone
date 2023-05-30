e0adf9800459c08c2606c8ddbb814c3687ac9b35
Evgenii Lukin <l40perl@gmail.com>
2023-05-30 23:12:43 +0600

    Added 4 buttons and configured their pressing in the tableViewCell.

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -41,7 +41,28 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath) as? TweetTableViewCell else {
             return UITableViewCell()
         }
-        
+        cell.delegate = self
         return cell
     }
+}
+
+//MARK: - TweetTableViewCellDelegate
+extension HomeViewController: TweetTableViewCellDelegate {
+    func tweetTableViewCellDidTapReplay() {
+        print("Replay")
+    }
+    
+    func tweetTableViewCellDidTapRetweet() {
+        print("Retweet")
+    }
+    
+    func tweetTableViewCellDidTapLike() {
+        print("Like")
+    }
+    
+    func tweetTableViewCellDidTapShare() {
+        print("Share")
+    }
+    
+    
 }
--- TwitterClone/Views/TweetTableViewCell.swift
+++ TwitterClone/Views/TweetTableViewCell.swift
@@ -7,9 +7,20 @@
 
 import UIKit
 
+protocol TweetTableViewCellDelegate: AnyObject {
+    func tweetTableViewCellDidTapReplay()
+    func tweetTableViewCellDidTapRetweet()
+    func tweetTableViewCellDidTapLike()
+    func tweetTableViewCellDidTapShare()
+}
+
 class TweetTableViewCell: UITableViewCell {
     
     static let identifier = "TweetTableViewCell"
+    
+    weak var delegate: TweetTableViewCellDelegate?
+    
+    private let actionSpacing: CGFloat = 60
     
     private let avatarImageView: UIImageView = {
         let imageView = UIImageView()
@@ -48,16 +59,78 @@ class TweetTableViewCell: UITableViewCell {
         return label
     }()
     
+    private let replayButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .systemGray2
+        button.setImage(UIImage(systemName: "bubble.left"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+       return button
+    }()
+    
+    private let retweetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .systemGray2
+        button.setImage(UIImage(systemName: "arrow.2.squarepath"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+       return button
+    }()
+    
+    private let likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .systemGray2
+        button.setImage(UIImage(systemName: "heart"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+       return button
+    }()
+    
+    private let shareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .systemGray2
+        button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+       return button
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(avatarImageView)
         contentView.addSubview(displayNameLabel)
         contentView.addSubview(usernameLabel)
         contentView.addSubview(tweetTextContentLabel)
+        contentView.addSubview(replayButton)
+        contentView.addSubview(retweetButton)
+        contentView.addSubview(likeButton)
+        contentView.addSubview(shareButton)
         
         configureConstraints()
+        configureButtons()
+    }
+    
+    @objc private func didTapReplay() {
+        delegate?.tweetTableViewCellDidTapReplay()
+    }
+    
+    @objc private func didTapRetweet() {
+        delegate?.tweetTableViewCellDidTapRetweet()
     }
 
+    @objc private func didTapLike() {
+        delegate?.tweetTableViewCellDidTapLike()
+    }
+
+    @objc private func didTapShare() {
+        delegate?.tweetTableViewCellDidTapShare()
+    }
+
+    //MARK: - configureButtons
+    func configureButtons() {
+        replayButton.addTarget(self, action: #selector(didTapReplay), for: .touchUpInside)
+        retweetButton.addTarget(self, action: #selector(didTapRetweet), for: .touchUpInside)
+        likeButton.addTarget(self, action: #selector(didTapLike), for: .touchUpInside)
+        shareButton.addTarget(self, action: #selector(didTapShare), for: .touchUpInside)
+    }
+    
+    //MARK: - configureConstraints
     private func configureConstraints() {
         let avatarImageViewConstraints = [
             avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
@@ -80,13 +153,37 @@ class TweetTableViewCell: UITableViewCell {
             tweetTextContentLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
             tweetTextContentLabel.topAnchor.constraint(equalTo: displayNameLabel.bottomAnchor, constant: 10),
             tweetTextContentLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
-            tweetTextContentLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15)
+        ]
+        
+        let replayButtonConstraints = [
+            replayButton.leadingAnchor.constraint(equalTo: tweetTextContentLabel.leadingAnchor),
+            replayButton.topAnchor.constraint(equalTo: tweetTextContentLabel.bottomAnchor, constant: 10),
+            replayButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15)
+        ]
+        
+        let retweetButtonConstraints = [
+            retweetButton.leadingAnchor.constraint(equalTo: replayButton.trailingAnchor, constant: actionSpacing),
+            retweetButton.centerYAnchor.constraint(equalTo: replayButton.centerYAnchor)
+        ]
+        
+        let likeButtonConstraints = [
+            likeButton.leadingAnchor.constraint(equalTo: retweetButton.trailingAnchor, constant: actionSpacing),
+            likeButton.centerYAnchor.constraint(equalTo: replayButton.centerYAnchor)
+        ]
+        
+        let shareButtonConstraints = [
+            shareButton.leadingAnchor.constraint(equalTo: likeButton.trailingAnchor, constant: actionSpacing),
+            shareButton.centerYAnchor.constraint(equalTo: replayButton.centerYAnchor)
         ]
 
         NSLayoutConstraint.activate(avatarImageViewConstraints)
         NSLayoutConstraint.activate(displayNameLabelConstraints)
         NSLayoutConstraint.activate(usernameLabelConstraints)
         NSLayoutConstraint.activate(tweetTextContentLabelConstraints)
+        NSLayoutConstraint.activate(replayButtonConstraints)
+        NSLayoutConstraint.activate(retweetButtonConstraints)
+        NSLayoutConstraint.activate(likeButtonConstraints)
+        NSLayoutConstraint.activate(shareButtonConstraints)
     }
     
     required init?(coder: NSCoder) {
